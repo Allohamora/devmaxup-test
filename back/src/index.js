57a97e2b0612a46e20a2936975e3b07932de623a
German <herman2000@yandex.ru>
2020-12-05 16:34:23 +0200

    Init for root

--- back/src/index.js
+++ back/src/index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const sequelize = require('./sequelize');
 const apiRouter = require('./controllers');
 const dotenv = require('dotenv');
+const path = require('path');
+const routeDecorator = require('./utils/routeDecorator');
 
 dotenv.config();
-const {PORT} = process.env;
+const { PORT, FRONT } = process.env;
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', apiRouter);
+
+if( FRONT ) {
+  const frontBuildPath = path.join(__dirname, '../../', 'front/build');
+  const indexHtmlPath = path.join(frontBuildPath, 'index.html');
+
+  app.use( express.static(frontBuildPath) );
+  app.get('*', routeDecorator(({res}) => res.sendFile(indexHtmlPath)));
+};
 
 (async () => {
   // force === drop old tables
